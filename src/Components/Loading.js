bea62411aed63ad96731d596a073fb89f1338789
mollikacomputer <mollikacomputer3@gmail.com>
2022-08-29 23:55:23 +0600

    trying to get radio loading

--- src/Components/Loading.js
+++ src/Components/Loading.js
@@ -1,26 +1,36 @@
 import { useState, CSSProperties } from "react";
 // import { CircleLoader } from "react-spinners";
 // import ClipLoader from "react-spinners/ClipLoader";
-import DotLoader from "react-spinners/ClipLoader";
+// import DotLoader from "react-spinners/ClipLoader";
+import Radio from "react-spinners/ClipLoader";
 
-const override: CSSProperties = {
-  display: "block",
-  margin: "0 auto",
-  borderColor: "black",
-};
+// const override: CSSProperties = {
+//   display: "block",
+//   margin: "0 auto",
+//   borderColor: "black",
+// };
 
 function Loading() {
-  let [loading, setLoading] = useState(true);
-  let [color, setColor] = useState("#ffffff");
+  // let [loading, setLoading] = useState(true);
+  // let [color, setColor] = useState("#ffffff");
 
   return (
-    <div className="sweet-loading">
-      {/* <button onClick={() => setLoading(!loading)}>Toggle Loader</button> */}
-      {/* <input value={color} onChange={(input) => setColor(input.target.value)} placeholder="Color of the loader" /> */}
+    // <div className="sweet-loading">
+    //   {/* <button onClick={() => setLoading(!loading)}>Toggle Loader</button> */}
+    //   {/* <input value={color} onChange={(input) => setColor(input.target.value)} placeholder="Color of the loader" /> */}
 
-      <DotLoader />
-      {/* <CircleLoader color={color} loading={loading} cssOverride={override} size={150} /> */}
-    </div>
+    //   {/* <DotLoader /> */}
+    //   {/* <CircleLoader color={color} loading={loading} cssOverride={override} size={150} /> */}
+
+    // </div>
+    <Radio
+    visible={true}
+    height="80"
+    width="80"
+    ariaLabel="radio-loading"
+    wrapperStyle={{}}
+    wrapperClass="radio-wrapper"
+  />
   );
 }
 
